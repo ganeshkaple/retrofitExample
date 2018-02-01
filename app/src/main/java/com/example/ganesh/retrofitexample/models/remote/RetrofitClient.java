c698dde241041d3aa0704188dcee4f8913470d75
Ganesh Kaple <gvklive@gmail.com>
2018-02-01 16:24:21 +0530

    https://code.tutsplus.com/tutorials/sending-data-with-retrofit-2-http-client-for-android--cms-27845 Using this, also tried to send data to server

--- app/src/main/java/com/example/ganesh/retrofitexample/models/remote/RetrofitClient.java
+++ app/src/main/java/com/example/ganesh/retrofitexample/models/remote/RetrofitClient.java
@@ -1,7 +1,6 @@
 package com.example.ganesh.retrofitexample.models.remote;
 
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -13,8 +12,8 @@ public class RetrofitClient {
     public static Retrofit getClient(String baseUrl) {
         if (retrofit == null) retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
-                //Trying RxJava Bitch
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                //Trying RxJava Bitch,Failed
+              //  .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit;
